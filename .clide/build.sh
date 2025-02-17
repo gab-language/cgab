7eae04494715a64fd5dd23d4eff9b914606558e0
Teddy Randby <tedrandby@gmail.com>
2025-02-17 16:21:21 -0500

    Final flags and comments for build

--- .clide/build.sh
+++ .clide/build.sh
@@ -56,25 +56,29 @@ function build {
   # allows users to download the released binaries and install everything
   # they need from there.
   #
-  # potentially need flag -rdynamic to export all libcgab symbols in the gab executable.
-  # Locally on linux, this doesn't seem to be a problem - loading shared objects at runtime
-  # doesn't require a second libcgab.so linked dynamically at runtime.
-  # Verified this with:
-  # strace -e trace=open,openat ./build-x86_64-linux-gnu/gab run test
+  # need flag -rdynamic to export all libcgab symbols in the gab executable.
+  # This way they are available to gab modules written in c which are loaded at runtime.
+  # any .dll or .so or .dylib modules, ie: everything built in the following step
   zig cc $flags $platform -rdynamic -o "build-$1/gab" "build-$1/libcgab.a" src/gab/*.c || exit 1
   echo "   Done!"
   echo "   $(file "build-$1/gab")"
 
   mkdir -p "build-$1/mod"
 
-  # Compile all src/mod c files into shared objects
-  # These modules link *dynamically* to libcgab - since they are loaded at runtime in gab process that already have all the necessary symbols.
-  # Linking statically here would be redundant.
+  # Compile all src/mod c files into shared objects / dlls
+  # These modules link *dynamically* to libcgab - they are loaded only at runtime by a process which is *already* exporting libcgab's symbols.
+  # There *could* be some versioning issues but we're ignoring that for now.
+  # The flag -undefined dynamic_lookup allows these symbols to remain undefined in the shared libraries we compile here. Normally, you'd
+  # have to specify that they link to cgab somehow (by including it statically or with -lcgab). We can just leave them undefined for this use
+  # case, and its actually ideal because of some nuances:
+  # - linking statically would cause each so to carry its own libcgab. This is 3.2M of code for an otherwise tiny (20k) module.
+  # - linking dynamically requires users to download libcgab.so, a dependency we are otherwise avoiding. This would defeat the purpose of
+  #   linking the gab binary statically - I want the user to download the latest binary and be able to run it to do *everything*
   echo "    Building shared libraries..."
   for file in src/mod/*.c; do
     name=$(basename "$file" ".c")
     echo "       Building gab$name..."
-    zig cc -shared $flags $platform -lcgab -o "build-$1/mod/c$name$dynlib_fileending" "$file" || exit 1
+    zig cc -shared -undefined dynamic_lookup $flags $platform -o "build-$1/mod/c$name$dynlib_fileending" "$file" || exit 1
     echo "       Done!"
     echo "       $(file "build-$1/mod/c$name$dynlib_fileending")"
   done
