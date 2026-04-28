bfdf95df7ebaddb86c18e8150846eca7275cd745
Teddy Randby <tedrandby@gmail.com>
2026-04-28 19:29:23 -0400

    Specify __cdecl everywhere

--- src/gab/main.c
+++ src/gab/main.c
@@ -201,7 +201,7 @@ int copy_file(FILE *in, FILE *out) {
  *   -> Or Maybe: 'github.com/gab-language/cgab@0.0.5' .use 'cstrings'
  */
 
-typedef union gab_value_pair (*dynlib_fn)(struct gab_triple);
+typedef union gab_value_pair (__cdecl *dynlib_fn)(struct gab_triple);
 
 union gab_value_pair gab_use_dynlib(struct gab_triple gab, const char *path,
                                     size_t len, const char **sargs,
